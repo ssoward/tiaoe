b345aac0e6ccc55c9892a5d15a47721bc8d6c192
ssoward <ssoward@wilsonelectronics.com>
2019-11-01 15:42:32 -0600

    test change.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function yo() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Edit <code>src/App.js</code> and save to reload. </p>
-        scott
+        test scott
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a>
       </header>
     </div>
